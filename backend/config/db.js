6336fb53ac5a5db09ea7ae167ec1a3af1b02c631
Kuldeep Jain <kuldipjain2002@gmail.com>
2023-07-19 09:24:29 +0530

    backend setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    mongoose.set("strictQuery", false);
     const conn = await mongoose.connect(process.env.Mongo_uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      //   useFindandModify: true,
     });
     console.log(`connection done: ${conn.connection.host}`);
   } catch (error) {
-    // console.log("oops");
+    console.log("oops");
     console.log(`error : ${error.message}`);
     process.exit();
   }
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const app = express();
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 dotenv.config();
 
@@ -13,16 +15,13 @@ connectDB();
 
 app.use(express.json()); //to accept json data
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`server created on port ${PORT}`));
-
 // app.get("/", (req, res) => {
 //   res.send("API / is running");
 // });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
-
+app.use("/api/message", messageRoutes);
 //if the upper routes dont work - these are the error handler.
 app.use(notFound);
 app.use(errorHandler);
@@ -35,3 +34,49 @@ app.use(errorHandler);
 //   const neededchat = chats.find((c) => c._id === req.params.id);
 //   res.send(neededchat);
 // });
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`server created on port ${PORT}`));
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    console.log("Connected to socket.io setup", userData._id);
+
+    socket.emit("connected", userData);
+    // console.log("Connected to socket.io");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  //   socket.on("typing", (room) => socket.in(room).emit("typing"));
+  //   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    console.log("new message", newMessageRecieved);
+    var chat = newMessageRecieved.chat;
+
+    if (!chat || !chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+      console.log("socket emit", user);
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
