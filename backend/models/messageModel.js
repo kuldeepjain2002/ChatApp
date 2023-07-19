7ceaaa13dbab007a3ba83bbfe60c34cf39875e03
Kuldeep Jain <kuldipjain2002@gmail.com>
2023-07-19 09:29:40 +0530

    project

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 const messageSchema = mongoose.Schema(
   {
     content: { type: String, trim: true },
-    sender: { type: mongoose.Schema.Types.objectId, ref: "User" },
-    chat: { type: mongoose.Schema.Types.objectId, ref: "Chat" },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
   },
   {
     timestamps: true,
   }
 );
 
-const Message = mongoose.model("Message", messagSchema);
+const Message = mongoose.model("Message", messageSchema);
 module.exports = Message;
